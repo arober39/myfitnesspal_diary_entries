ec0ba19fda51ad9972a56d6b923e5867fcab28f8
alexis roberson <akroberson18@gmail.com>
2023-07-27 23:46:49 -0500

    simplified function for extracting meal name

--- myfitnesspal_to_elasticsearch.py
+++ myfitnesspal_to_elasticsearch.py
@@ -24,9 +24,9 @@ client = myfitnesspal.Client()
 
 # send breakfast, lunch, dinner structs to JSON file
 def send_meals_to_json_file(json_object):
-    meals_json_object = json.dumps(json_object, indent=4)
+    meals_json_object = json.dumps(json_object)
     meals_json = json.loads(meals_json_object)
-    
+
     with open("meals_macros_calories.json", "a") as outfile:
         outfile.write(meals_json + ',')
 
@@ -56,18 +56,6 @@ def send_todays_total_to_elasticsearch(total_daily_entry):
         document=total_daily_entry
     )
 
-def total_daily_macros_and_calories_output(date, daily_totals_dict):
-    daily_total_dict = {
-        'date': date,
-        'daily_macros': daily_totals_dict
-    }
-    # uncomment one of lines below when to send 
-    # total macros and calories to either
-    # elasticsearch index or JSON file
-
-    # send_todays_total_to_elasticsearch(daily_total_dict)
-    # send_todays_total_to_json_file(daily_total_dict)
-
 def each_meals_macros_and_calories_output(breakfast_struct, lunch_struct, dinner_struct):
     breakfast_json_object = json.dumps(breakfast_struct)
     # send_meals_to_json_file(breakfast_json_object)
@@ -82,22 +70,22 @@ def each_meals_macros_and_calories_output(breakfast_struct, lunch_struct, dinner
     # send_meals_to_elasticsearch(dinner_json_object)
 
 # -----------------------------------------------------
-#        Combine data outputted from myfitnesspal 
-#              client into a single struct
+#        Prepare myfitnesspal data to send to ES
+#                      or JSON file
 # -----------------------------------------------------
 
 def extract_name_of_meal(meal_string):
-    # extract meals
-    spl_word = '{'
+    # extract meal name
     spl_word2 = ','
+    char_list = ['-', '(', ')']
 
-    meal_split_at_bracket = meal_string.partition(spl_word)[0]
     meal_split_at_comma = meal_string.partition(spl_word2)[0]
-    meal_remove_special_chars = meal_split_at_comma.replace('-', '')
-    meal_remove_closing_parenthesis = meal_remove_special_chars.replace(')', '')
-    meal_remove_opening_parenthesis = meal_remove_closing_parenthesis.replace('(', '')
-    meal_name = " ".join(meal_remove_opening_parenthesis.split())
 
+    for i in meal_split_at_comma:
+        if i in char_list:
+            meal_split_at_comma = meal_split_at_comma.replace(i, ' ')
+
+    meal_name = " ".join(meal_split_at_comma.split())
     return meal_name
 
 def grab_inner_dict_for_macros(foods, item, item_name, outer_outer):
@@ -112,28 +100,27 @@ def grab_inner_dict_for_macros(foods, item, item_name, outer_outer):
 
     return temp_dict
 
-def parse_each_meal_for_extraction(foods, date, meal_type, totals):
+def parse_each_meal_for_extraction(date, meal_type, totals):
     list_of_foods = []
 
     # outer dict
     outer_dict = {
         'date': date,
         'meal_type': meal_type['_name'],
-        'total_macros': totals,
+        'total_meal_macros': totals,
         'list_of_food_macros': [],
         'list_of_foods': []
     }
-    
+    entries = meal_type['_entries']
     outer_outer_dict = {}
     macr = []
-    for i in range(len(foods)):
+    for i in range(len(entries)):
         # extract food name
-        food_string = str(foods[i])
-        food_name = extract_name_of_meal(food_string)
+        food_name = extract_name_of_meal(str(entries[i]))
         list_of_foods.append(food_name)
 
         # create inner struct for each food
-        macro_dict = grab_inner_dict_for_macros(foods, i, food_name, outer_dict)
+        macro_dict = grab_inner_dict_for_macros(entries, i, food_name, outer_dict)
         outer_dict['list_of_food_macros'].append(macro_dict)
 
     outer_dict['list_of_foods'] = list_of_foods
@@ -141,56 +128,48 @@ def parse_each_meal_for_extraction(foods, date, meal_type, totals):
     return outer_dict
 
 
-def structure_nutrition_data(year, month, day, date):
-    if month[0] == '0':
-        month = month[1]
+def structure_nutrition_data(dates):
+    # iterate over list of dates
+    for date in dates:
+        # get the meals for the day
+        myfitnesspal_day = client.get_date(date.year, date.month, date.day)
 
-    # get the meals per day
-    myfitnesspal_day = client.get_date(int(year), int(month), int(day))
+        if myfitnesspal_day:
+            date_string = str(date.year) + '-' + str(date.month) + '-' + str(date.day)
 
-    if myfitnesspal_day:
-        # get total calories and macros for the day
-        total_daily_macros_and_calories_output(date, myfitnesspal_day.totals)
+            # send total calories and macros for the entire day to output function
+            daily_total_dict = {
+                'date': date_string,
+                'daily_macros': myfitnesspal_day.totals
+            }
+        
+            # send_todays_total_to_elasticsearch(daily_total_dict)
+            send_todays_total_to_json_file(daily_total_dict)
 
-        # get calories and macros for each meal of the day
-        breakfast = myfitnesspal_day.meals[0]
-        lunch = myfitnesspal_day.meals[1]
-        dinner = myfitnesspal_day.meals[2]
-        breakfast_vars = vars(myfitnesspal_day.meals[0])
-        lunch_vars = vars(myfitnesspal_day.meals[1])
-        dinner_vars = vars(myfitnesspal_day.meals[2])
-        breakfast_foods = breakfast.entries
-        lunch_foods = lunch.entries
-        dinner_foods = dinner.entries
-
-        breakfast_dict = parse_each_meal_for_extraction(breakfast_foods, date, breakfast_vars, breakfast.totals)
-        lunch_dict = parse_each_meal_for_extraction(lunch_foods, date, lunch_vars, lunch.totals)
-        dinner_dict = parse_each_meal_for_extraction(dinner_foods, date, dinner_vars, dinner.totals)
-
-        # send each meal dict to outputting function
-        each_meals_macros_and_calories_output(breakfast_dict, lunch_dict, dinner_dict)
-    else:
-        print('found an empty struct')
-        return
-
-def parse_range_of_dates(dates):
-    for item in dates:
-        year = item[:4]
-        month = item[5:7]
-        day = item[8:]
-        structure_nutrition_data(year, month, day, item)
+            # get calories and macros for meals
+            breakfast = myfitnesspal_day.meals[0]
+            lunch = myfitnesspal_day.meals[1]
+            dinner = myfitnesspal_day.meals[2]
+            if breakfast and lunch and dinner:
+                breakfast_dict = parse_each_meal_for_extraction(date_string, vars(breakfast), breakfast.totals)
+                lunch_dict = parse_each_meal_for_extraction(date_string, vars(lunch), lunch.totals)
+                dinner_dict = parse_each_meal_for_extraction(date_string, vars(dinner), dinner.totals)
+           
+            # send macros and calories for each meal to output function
+            each_meals_macros_and_calories_output(breakfast_dict, lunch_dict, dinner_dict)
 
 def main():
-    start_date = date(2020, 10, 1)
-    end_date = date(2020, 10, 4)
-
+    
+    start_date = date(2020, 12, 25)
+    end_date = date(2021, 1, 3)
+    
     delta = timedelta(days=1)
     dates = []
     while start_date <= end_date:
-        dates.append(start_date.isoformat())
+        dates.append(start_date)
         start_date += delta
 
-    parse_range_of_dates(dates)
+    structure_nutrition_data(dates)
 
 if __name__ == "__main__":
     main() 
