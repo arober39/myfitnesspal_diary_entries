35cc03660e12dfc72918a8802019fa77f040b83a
alexis roberson <akroberson18@gmail.com>
2023-07-28 14:53:35 -0500

    simplfied meal extraction function, added snacks meals

--- myfitnesspal_to_elasticsearch.py
+++ myfitnesspal_to_elasticsearch.py
@@ -1,19 +1,20 @@
-import json, jsonlines
+
 import myfitnesspal
 from datetime import date, timedelta
 from elasticsearch import Elasticsearch
+import json
 import configparser
 
 config = configparser.ConfigParser()
 config.read('mfp_elastic.ini')
 
 # Elasticsearch client instance
-es = Elasticsearch(
+es_client = Elasticsearch(
     cloud_id=config['ELASTIC']['cloud_id'],
     basic_auth=("elastic", config['ELASTIC']['password'])
 )
 
-es.info()
+es_client.info()
 
 # myfitnesspal client
 client = myfitnesspal.Client()
@@ -44,30 +45,17 @@ def send_todays_total_to_json_file(json_object):
 
 # send breakfast, lunch, dinner structs to ES
 def send_meals_to_elasticsearch(meals_diary_entry):
-    es.index(
-        index='myfitnesspal_index',
+    es_client.index(
+        index='myfitnesspal_diary_official',
         document=meals_diary_entry
     )
 
 # send each days overall calories and macros to ES
 def send_todays_total_to_elasticsearch(total_daily_entry):
-    es.index(
+    es_client.index(
         index='daily_myfitnesspal_total_index',
         document=total_daily_entry
     )
-
-def each_meals_macros_and_calories_output(breakfast_struct, lunch_struct, dinner_struct):
-    breakfast_json_object = json.dumps(breakfast_struct)
-    # send_meals_to_json_file(breakfast_json_object)
-    # send_meals_to_elasticsearch(breakfast_json_object)
-
-    lunch_json_object = json.dumps(lunch_struct)
-    # send_meals_to_json_file(lunch_json_object)
-    # send_meals_to_elasticsearch(lunch_json_object)
-
-    dinner_json_object = json.dumps(dinner_struct)
-    # send_meals_to_json_file(dinner_json_object)
-    # send_meals_to_elasticsearch(dinner_json_object)
 
 # -----------------------------------------------------
 #        Prepare myfitnesspal data to send to ES
@@ -76,10 +64,10 @@ def each_meals_macros_and_calories_output(breakfast_struct, lunch_struct, dinner
 
 def extract_name_of_meal(meal_string):
     # extract meal name
-    spl_word2 = ','
+    spl_word = ','
     char_list = ['-', '(', ')']
 
-    meal_split_at_comma = meal_string.partition(spl_word2)[0]
+    meal_split_at_comma = meal_string.partition(spl_word)[0]
 
     for i in meal_split_at_comma:
         if i in char_list:
@@ -88,7 +76,7 @@ def extract_name_of_meal(meal_string):
     meal_name = " ".join(meal_split_at_comma.split())
     return meal_name
 
-def grab_inner_dict_for_macros(foods, item, item_name, outer_outer):
+def grab_inner_dict_for_macros(foods, item, item_name):
     temp_dict = {
         'food_macros': {}
     }
@@ -100,30 +88,27 @@ def grab_inner_dict_for_macros(foods, item, item_name, outer_outer):
 
     return temp_dict
 
-def parse_each_meal_for_extraction(date, meal_type, totals):
-    list_of_foods = []
-
-    # outer dict
+def parse_each_meal_for_extraction(date, meal, meal_totals):
     outer_dict = {
         'date': date,
-        'meal_type': meal_type['_name'],
-        'total_meal_macros': totals,
+        'meal_type': meal['_name'],
+        'total_meal_macros': meal_totals,
         'list_of_food_macros': [],
         'list_of_foods': []
     }
-    entries = meal_type['_entries']
-    outer_outer_dict = {}
-    macr = []
+    food_names_lst = []
+    entries = meal['_entries']
+  
     for i in range(len(entries)):
         # extract food name
         food_name = extract_name_of_meal(str(entries[i]))
-        list_of_foods.append(food_name)
+        food_names_lst.append(food_name)
 
         # create inner struct for each food
-        macro_dict = grab_inner_dict_for_macros(entries, i, food_name, outer_dict)
+        macro_dict = grab_inner_dict_for_macros(entries, i, food_name)
         outer_dict['list_of_food_macros'].append(macro_dict)
 
-    outer_dict['list_of_foods'] = list_of_foods
+    outer_dict['list_of_foods'] = food_names_lst
 
     return outer_dict
 
@@ -144,24 +129,41 @@ def structure_nutrition_data(dates):
             }
         
             # send_todays_total_to_elasticsearch(daily_total_dict)
-            send_todays_total_to_json_file(daily_total_dict)
+            # send_todays_total_to_json_file(daily_total_dict)
 
             # get calories and macros for meals
             breakfast = myfitnesspal_day.meals[0]
             lunch = myfitnesspal_day.meals[1]
             dinner = myfitnesspal_day.meals[2]
-            if breakfast and lunch and dinner:
+            snacks = myfitnesspal_day.meals[3]
+
+            if breakfast and lunch and dinner and snacks:
                 breakfast_dict = parse_each_meal_for_extraction(date_string, vars(breakfast), breakfast.totals)
                 lunch_dict = parse_each_meal_for_extraction(date_string, vars(lunch), lunch.totals)
                 dinner_dict = parse_each_meal_for_extraction(date_string, vars(dinner), dinner.totals)
-           
+                snacks_dict = parse_each_meal_for_extraction(date_string, vars(snacks), snacks.totals)
+
             # send macros and calories for each meal to output function
-            each_meals_macros_and_calories_output(breakfast_dict, lunch_dict, dinner_dict)
+            breakfast_json_object = json.dumps(breakfast_dict)
+            # send_meals_to_json_file(breakfast_json_object)
+            send_meals_to_elasticsearch(breakfast_json_object)
+
+            lunch_json_object = json.dumps(lunch_dict)
+            # send_meals_to_json_file(lunch_json_object)
+            send_meals_to_elasticsearch(lunch_json_object)
+
+            dinner_json_object = json.dumps(dinner_dict)
+            # send_meals_to_json_file(dinner_json_object)
+            send_meals_to_elasticsearch(dinner_json_object)
+        
+            snacks_json_object = json.dumps(snacks_dict)
+            # send_meals_to_json_file(snack_json_object)
+            send_meals_to_elasticsearch(snacks_json_object)
 
 def main():
     
-    start_date = date(2020, 12, 25)
-    end_date = date(2021, 1, 3)
+    start_date = date(2020, 10, 1)
+    end_date = date(2021, 5, 1)
     
     delta = timedelta(days=1)
     dates = []
